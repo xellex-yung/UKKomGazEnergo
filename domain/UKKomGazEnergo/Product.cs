2eb6caa181636a1ffe4990f30b8d1c3040e6afd3
Alexey Seryogin <xellex.yung@yandex.ru>
2021-02-13 00:42:27 +0300

    Describe entity product

---
+++ domain/UKKomGazEnergo/Product.cs
@@ -0,0 +1,14 @@
+﻿
+namespace UKKomGazEnergo
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; }
+        public Product(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+    }
+}
