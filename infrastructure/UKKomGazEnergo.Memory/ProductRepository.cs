f48331339307e08aaf3afc90581feaafbeee650d
Alexey Seryogin <xellex.yung@yandex.ru>
2021-02-13 01:09:41 +0300

    Add a simple in-memory product repository implementation

---
+++ infrastructure/UKKomGazEnergo.Memory/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace UKKomGazEnergo.Memory
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly Product[] products = new[]
+        {
+            new Product(1,"OneSomeThing"),
+            new Product(2,"TwoSomeThing"),
+            new Product(3,"ThreeSomeThing"),
+        };
+        public Product[] GetAllByTitle(string titlePart)
+        {
+            return products.Where(products => products.Title.Contains(titlePart)).ToArray();
+        }
+    }
+}
