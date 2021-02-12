4a963dc567792dfdab2da10dc94f8b611b773389
Alexey Seryogin <xellex.yung@yandex.ru>
2021-02-13 01:21:49 +0300

    Create web application

--- domain/UKKomGazEnergo/IProductRepository.cs
+++ domain/UKKomGazEnergo/IProductRepository.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace UKKomGazEnergo
 {
-    interface IProductRepository
+    public interface IProductRepository
     {
+        Product[] GetAllByTitle(string titlePart);
     }
 }
