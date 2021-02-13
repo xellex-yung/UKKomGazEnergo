1fd127420a226658432e31ae076fecfef9f23ec9
Alexey Seryogin <xellex.yung@yandex.ru>
2021-02-13 10:34:17 +0300

    Modify web start up and .csproj, first view; Create search controller and new search view.

---
+++ presentation/UKKomGazEnergo.Web/Controllers/SearchController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UKKomGazEnergo.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public SearchController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public IActionResult Index(string query)
+        {
+            var products = productRepository.GetAllByTitle(query);
+            return View(products);
+        }
+    }
+}
--- presentation/UKKomGazEnergo.Web/Startup.cs
+++ presentation/UKKomGazEnergo.Web/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using UKKomGazEnergo.Memory;
 
 namespace UKKomGazEnergo.Web
 {
@@ -24,6 +25,7 @@ namespace UKKomGazEnergo.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSingleton<IProductRepository, ProductRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
