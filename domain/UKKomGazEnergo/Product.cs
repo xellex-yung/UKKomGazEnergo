2f420db552cf408b19aa6064ca676281c769c217
Alexey Seryogin <xellex.yung@yandex.ru>
2021-02-16 12:09:58 +0300

    Update product.cs

--- domain/UKKomGazEnergo/Product.cs
+++ domain/UKKomGazEnergo/Product.cs
@@ -3,12 +3,26 @@ namespace UKKomGazEnergo
 {
     public class Product
     {
+
         public int Id { get; set; }
         public string Title { get; }
-        public Product(int id, string title)
+        public string  Country { get; }
+        public string Manufacturer { get; }
+        public string Description{ get; set; }
+        public decimal Price { get; set; }
+        public Product(int id, string title, string country, string manufacturer, string description, decimal price)
         {
             Id = id;
             Title = title;
+            Country = country;
+            Manufacturer = manufacturer;
+            Description = description;
+            Price = price;
+        }
+
+        internal static bool IsCountry(string query)
+        {
+            return string.IsNullOrEmpty(query);
         }
     }
 }
